39374950f96cf528349f290ddbad296dc8daab86
RaulAlvaradoT <ingraulalvarado@hotmail.com>
2023-05-14 21:23:04 -0600

    Se agrega video

--- resources/views/users/compañia.blade.php
+++ resources/views/users/compañia.blade.php
@@ -2,7 +2,8 @@
 
 @section('contenido')
     <div style="text-align: center;">
-        <h1 style="text-align: center; margin: 5%; font-size: 300%">StellarHost</h1>
-        <iframe width="560" height="315" src="https://youtu.be/4cAXHgODhDU"
-        frameborder="0" allowfullscreen></iframe>
-@endsection
+        {{-- <h1 style="text-align: center; margin: 5%; font-size: 300%">StellarHost</h1> --}}
+        <iframe style="margin: 1%; margin-top: 7%; justify-content: center; align-items: center;" width="715" height="402"
+            src="https://www.youtube.com/embed/4cAXHgODhDU" title="StellarHost" frameborder="0"
+            allow="accelerometer; autoplay; web-share" allowfullscreen></iframe>
+    @endsection
