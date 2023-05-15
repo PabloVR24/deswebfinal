39c568156f43a7162f5bd8cbbeacf95fda1172e4
PabloVR24 <pavara_2010@hotmail.com>
2023-05-15 00:23:18 -0600

    Chatbot y demas

--- resources/views/users/compañia.blade.php
+++ resources/views/users/compañia.blade.php
@@ -3,6 +3,9 @@
 @section('contenido')
     <div style="text-align: center;">
         <h1 style="text-align: center; margin: 5%; font-size: 300%">StellarHost</h1>
-        <iframe width="560" height="315" src="https://youtu.be/4cAXHgODhDU"
-        frameborder="0" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/4cAXHgODhDU" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>
 @endsection
