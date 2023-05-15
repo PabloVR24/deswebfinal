c31e6318ded4ce83a93b4fb60362e204b1d5ecb1
PabloVR24 <pavara_2010@hotmail.com>
2023-05-14 21:09:37 -0600

    a

--- resources/views/admin/sugerencias/index.blade.php
+++ resources/views/admin/sugerencias/index.blade.php
@@ -1,19 +1,9 @@
 @extends('admin.navbar')
-
+@extends('extends.datatables')
 @section('contenido')
-    <link href="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.13.4/r-2.4.1/datatables.min.css" rel="stylesheet" />
-    <script src="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.13.4/r-2.4.1/datatables.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#alumnosTable').DataTable({
-                responsive: true
-            });
-        });
-    </script>
-
     <div class="container">
         <table>
-            <table id="alumnosTable" class="display">
+            <table id="sugerenciasTable" class="display">
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -34,4 +24,19 @@
                 </tbody>
             </table>
     </div>
+
+    <style>
+        table th {
+            background-color: red;
+            color: white;
+        }
+
+        table>tbody>tr>td {
+            vertical-align: middle !important;
+        }
+
+        .btn-group .btn-group-vertical {
+            position: absolute !important;
+        }
+    </style>
 @endsection
